0a290fd0e9279cc42565a3453a0dec7921fe941f
David Terés <david.teres@hotmail.com>
2019-03-12 08:52:03 +0100

    udpclientpython

---
+++ P2/UDPPythonClient.py
@@ -0,0 +1,11 @@
+from socket import *
+
+serverName='172.20.2.2'
+serverPort=12000
+clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
+message = raw_input("Input lowercase sentence: ")
+messageLowerCase = message.lower()
+clientSocket.sendto(messageLowerCase,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifeiedMessage
+clientSocket.close()
