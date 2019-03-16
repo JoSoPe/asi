301fc074a1c2ac878a3c75417ec3e38221f178ac
JoSoPe <26833958+JoSoPe@users.noreply.github.com>
2019-03-16 10:26:29 +0100

    estats client afegits

--- P2/TCPPythonServerClient.py
+++ P2/TCPPythonServerClient.py
@@ -3,11 +3,12 @@
 import socket
 import sys
 
-HOST = None               # Symbolic name meaning all available interfaces
+
 PORT = 50007              # Arbitrary non-privileged port
 s = None
-print 'Waiting Client'
 def E0():
+    print 'State 0 opening socket'
+    HOST = None               # Symbolic name meaning all available interfaces
     for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
                                   socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
         af, socktype, proto, canonname, sa = res
@@ -29,9 +30,10 @@ def E0():
         sys.exit(1)
         E0()
     else:
-        E1()
+        E1(s)
 
-def E1():
+def E1(s):
+    print 'State 1 socket opened, waiting a client'
     conn, addr = s.accept()
     print 'Connected by', addr
     received = True
@@ -45,6 +47,45 @@ def E1():
             break
         missatge=raw_input('m: ')
         conn.send(missatge)
-        conn.close()
-        E0()    
-E0()
+    conn.close()
+    E1(s)
+
+def E2():
+    print 'State2 client mode opening socket'
+    HOST = raw_input('C: ')    # The remote host "172.20.2.1"
+    s = None
+    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            s = socket.socket(af, socktype, proto)
+        except socket.error as msg:
+            s = None
+            continue
+        try:
+            s.connect(sa)
+        except socket.error as msg:
+            s.close()
+            s = None
+            continue
+        break
+    if s is None:
+        print 'could not open socket'
+        sys.exit(1)
+        E2()
+    connected = True
+    E3(s,connected)
+
+def E3(s,connected):
+    print 'state3 soket opened properly, waiting message to send'
+    while connected:
+            MSG = raw_input('M: ')
+            s.sendall(MSG)
+            data = s.recv(1024)
+            if data == 'D':
+                connected = False
+                print repr(data) , 'Properly disconected '
+                break
+            print 'M->', repr(data)
+    s.close()
+    E0()
+E2()
