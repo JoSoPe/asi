6222e35f75a8c7cb782f87837d21d7e5116d39f2
JoSoPe <26833958+JoSoPe@users.noreply.github.com>
2019-03-16 09:43:52 +0100

    primera versió stats

--- P2/TCPPythonServerClient.py
+++ P2/TCPPythonServerClient.py
@@ -7,36 +7,44 @@ HOST = None               # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = None
 print 'Waiting Client'
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
-                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        s = None
-        continue
-    try:
-        s.bind(sa)
-        s.listen(1)
-    except socket.error as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print 'could not open socket'
-    sys.exit(1)
-conn, addr = s.accept()
-print 'Connected by', addr
-received = True
-while received:
-    data = conn.recv(1024)
-    print data
-    if data == 'D':
-        received=False
-        conn.send('D')
-        print 'Client ',addr,' disconnect'
+def E0():
+    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
+                                  socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+        af, socktype, proto, canonname, sa = res
+        try:
+            s = socket.socket(af, socktype, proto)
+        except socket.error as msg:
+            s = None
+            continue
+        try:
+            s.bind(sa)
+            s.listen(1)
+        except socket.error as msg:
+            s.close()
+            s = None
+            continue
         break
-    missatge=raw_input('m: ')
-    conn.send(missatge)
-conn.close()
+    if s is None:
+        print 'could not open socket'
+        sys.exit(1)
+        E0()
+    else:
+        E1()
+
+def E1():
+    conn, addr = s.accept()
+    print 'Connected by', addr
+    received = True
+    while received:
+        data = conn.recv(1024)
+        print data
+        if data == 'D':
+            received=False
+            conn.send('D')
+            print 'Client ',addr,' disconnect'
+            break
+        missatge=raw_input('m: ')
+        conn.send(missatge)
+        conn.close()
+        E0()    
+E0()
